c68da60deef24edb28198f2e5286e713f511d33d
James <metissec@users.noreply.github.com>
2014-12-25 20:27:35 -0500

    Update hilbert.py

--- hilbert.py
+++ hilbert.py
@@ -4,26 +4,36 @@ class Hilbert():
         self.y = 0     
         self._step(0)
         self.iter = self._hil(0, 1, order)
+    
     #delegate iterator
     def __iter__(self):
         return self
+    
     def __next__(self):
         return next(self.iter)
+    
     def _hil(self, dirs, rot, order):
+    
             if (order == 0): 
                 return
+    
             dirs += rot
             yield from self._hil(dirs, -rot, (order-1))
             yield from self._step(dirs)
+    
             dirs -= rot
             yield from self._hil(dirs, rot, (order-1))
             yield from self._step(dirs)
             yield from self._hil(dirs, rot, (order-1))
+    
             dirs -= rot
             yield from self._step(dirs)
             yield from self._hil(dirs, -rot, (order-1))
+    
     def _step(self, dirs):
+    
         dirs %= 4
+    
         if dirs == 0:
             self.x += 1
         elif dirs == 1:
@@ -32,6 +42,7 @@ class Hilbert():
             self.x -= 1
         else:
             self.y -= 1 
+    
         yield self.x, self.y
         
     
